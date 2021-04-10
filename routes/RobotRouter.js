3c03f9271d28e2ab1e1b44601abcd5dfa92945cb
Caline Doumit <calinedoumit@gmail.com>
2021-04-10 03:46:36 +0200

    get robots that are not occupied and patients not active

--- routes/RobotRouter.js
+++ routes/RobotRouter.js
@@ -44,6 +44,19 @@ robotRouter.route('/')
             .catch((err) => next(err));
     });
 
+robotRouter.route('/RobotIsNotOccupied')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+    .get(cors.cors,(req,res,next) => {
+        Robots.find({isOccupied:false})
+            .then((robots) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(robots);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+
+
 
 
 
